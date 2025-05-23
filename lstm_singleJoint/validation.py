0391be3c6a700c20ffe83eaff00b081a906124b8
Grayson <grasnyder2001@gmail.com>
2025-05-23 11:29:48 -0500

    Tuned period plotting for j42

--- lstm_singleJoint/validation.py
+++ lstm_singleJoint/validation.py
@@ -15,8 +15,8 @@ therapist = pd.read_csv('X2_SRA_B_07-05-2024_10-41-46-mod-sync.csv',
                        nrows=894640-696306)
 
 # Prepare data
-patient_data = patient[['JointPositions_3']].values.astype('float32')
-therapist_data = therapist[['JointPositions_1']].values.astype('float32')
+patient_data = patient[['JointPositions_4']].values.astype('float32')
+therapist_data = therapist[['JointPositions_2']].values.astype('float32')
 timeseries = np.column_stack((patient_data, therapist_data))
 
 # Validation split
@@ -46,7 +46,7 @@ class JointModel(nn.Module):
 
 # Load model
 model = JointModel()
-model.load_state_dict(torch.load('trial5/j31/lstm_model_epoch189.pth'))
+model.load_state_dict(torch.load('trial5/j42/lstm_model_epoch125.pth'))
 model.eval()
 
 # Validation
@@ -117,9 +117,9 @@ plt.show()
 
 # Overlay of periodic data
 # Find peaks in the therapist data and divide into periodic segments
-data_peaks, _ = find_peaks(therapist_true, height=0.4, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=?
+data_peaks, _ = find_peaks(-therapist_true, height=1.0, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
 periodic_data = [therapist_true[data_peaks[i]:data_peaks[i+1]] for i in range(len(data_peaks)-1)]
-pred_peaks, _ = find_peaks(therapist_pred, height=0.4, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=?
+pred_peaks, _ = find_peaks(-therapist_pred, height=1.0, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
 periodic_pred = [therapist_pred[pred_peaks[i]:pred_peaks[i+1]] for i in range(len(pred_peaks)-1)]
 
 # Normalize periodic data
