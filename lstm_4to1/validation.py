2b23894c83ad387e957c876477c1faab12191fee
Grayson <grasnyder2001@gmail.com>
2025-05-30 10:41:47 -0500

    Added period error plotting and rearranged code to condense plotting to one location

--- lstm_4to1/validation.py
+++ lstm_4to1/validation.py
@@ -16,7 +16,7 @@ therapist = pd.read_csv('X2_SRA_B_07-05-2024_10-41-46-mod-sync.csv',
 
 # Prepare data
 patient_data = patient[['JointPositions_1', 'JointPositions_2', 'JointPositions_3', 'JointPositions_4']].values.astype('float32')
-therapist_data = therapist[['JointPositions_3']].values.astype('float32')
+therapist_data = therapist[['JointPositions_1']].values.astype('float32')
 timeseries = np.column_stack((patient_data, therapist_data))
 
 # Validation split
@@ -55,7 +55,7 @@ class JointModel(nn.Module):
 
 # Load model
 model = JointModel()
-model.load_state_dict(torch.load('trial1/lstm_model_epoch12.pth'))
+model.load_state_dict(torch.load('trial2/j1/lstm_model_epoch32.pth'))
 model.eval()
 
 # Validation
@@ -75,15 +75,6 @@ with torch.no_grad():
     # Pointwise errors (signed)
     errors = (y_pred2 - y_true).numpy()  # Convert to numpy for plotting
 
-# Plot histogram of errors
-plt.figure(figsize=(12, 6))
-plt.hist(errors, bins=50, alpha=0.7, color='blue')
-plt.xlabel('Error (Radians)')
-plt.ylabel('# of Occurrences')
-plt.title(f'Distribution of Prediction Errors, RMSE: {valid_rmse:.4f}, Max: {errors.max():.4f}, Std Dev: {errors.std():.4f}, Mean: {errors.mean():.4f}')
-plt.grid(True)
-plt.show()
-
 # Plotting
 with torch.no_grad():
     # Create plotting arrays
@@ -100,35 +91,11 @@ with torch.no_grad():
     # Fill predictions (aligned with true values)
     therapist_pred[lookback:lookback+len(valid_pred)] = valid_pred
 
-# Plot only validation portion
-plt.figure(figsize=(12, 6))
-plt.plot(therapist_true, c='b', label='True Therapist Data')
-plt.plot(therapist_pred, c='r', linestyle='--', label='Predicted Therapist Data')
-# plt.plot(valid[:, 0], c='g', alpha=0.75, label='Patient Data (input)')
-plt.xlim(0, 7500)
-plt.xlabel('Time Steps (~4ms)')
-plt.ylabel('Joint Positions (Radians)')
-plt.legend()
-plt.title("Validation: Therapist Hip Prediction from Patient Data")
-plt.show()
-# plt.savefig('lstm_therapist_prediction.png', dpi=300, bbox_inches='tight')
-
-# # Plot error over time
-# plt.figure(figsize=(12, 6))
-# plt.plot(errors, c='r', label='Prediction Error')
-# plt.xlim(0, 7500)
-# plt.xlabel('Time Steps (~4ms)')
-# plt.ylabel('Joint Positions Errors (Radians)')
-# plt.legend()
-# plt.title("Error over Time")
-# plt.show()
-
-
 # Overlay of periodic data
 # Find peaks in the therapist data and divide into periodic segments
-data_peaks, _ = find_peaks(therapist_true, height=0.9, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
+data_peaks, _ = find_peaks(therapist_true, height=0.6, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
 periodic_data = [therapist_true[data_peaks[i]:data_peaks[i+1]] for i in range(len(data_peaks)-1)]
-pred_peaks, _ = find_peaks(therapist_pred, height=0.9, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
+pred_peaks, _ = find_peaks(therapist_pred, height=0.6, distance=1000)  # ONLY NEGATIVE FOR KNEES (2,4), Heights: J13=1.0, J24=1.4, J31=0.4, J42=1.0
 periodic_pred = [therapist_pred[pred_peaks[i]:pred_peaks[i+1]] for i in range(len(pred_peaks)-1)]
 
 # Normalize periodic data
@@ -156,12 +123,57 @@ mean_pred = np.mean(stacked_pred, axis=0)
 std_data = np.std(stacked_data, axis=0)
 std_pred = np.std(stacked_pred, axis=0)
 
+# Get errors for periodic data
+mean_err = mean_data - mean_pred
+std_err = std_data - std_pred
+
+# ALL PLOTTING HAPPENS BELOW HERE. READ INSTRUCTIONS FOR CREATING BEST PLOTS
+# Plot histogram of errors
+# plt.figure(figsize=(12, 6))
+# plt.hist(errors, bins=50, alpha=0.7, color='blue')
+# plt.xlabel('Error (Radians)')
+# plt.ylabel('# of Occurrences')
+# plt.title(f'Distribution of Prediction Errors, RMSE: {valid_rmse:.4f}, Max: {errors.max():.4f}, Std Dev: {errors.std():.4f}, Mean: {errors.mean():.4f}')
+# plt.grid(True)
+# plt.show()
+
+# Plot only validation data with prediction overlay
+# Change xlim for desired time steps
+# Limit to 7500 time steps (~30 seconds) EXCEPT when determining amplitude for periodic plots
+# plt.figure(figsize=(12, 6))
+# plt.plot(therapist_true, c='b', label='True Therapist Data')
+# # plt.plot(therapist_pred, c='r', linestyle='--', label='Predicted Therapist Data')
+# # plt.plot(valid[:, 0], c='g', alpha=0.75, label='Patient Data (input)')
+# plt.xlim(0, 7500)
+# plt.xlabel('Time Steps (~4ms)')
+# plt.ylabel('Joint Positions (Radians)')
+# plt.legend()
+# plt.title("Validation: Therapist Hip Prediction from Patient Data")
+# plt.show()
+# # plt.savefig('lstm_therapist_prediction.png', dpi=300, bbox_inches='tight')
+
+# # Plot error over time
+# plt.figure(figsize=(12, 6))
+# plt.plot(errors, c='r', label='Prediction Error')
+# plt.xlim(0, 7500)
+# plt.xlabel('Time Steps (~4ms)')
+# plt.ylabel('Joint Positions Errors (Radians)')
+# plt.legend()
+# plt.title("Error over Time")
+# plt.show()
+
 # Plot normalized periodic data
 plt.figure(figsize=(12, 6))
+
+# Plots each period as a separate line. Use to look for outliers in amplitude
+# Missteps can cause outliers where multiple periods will be combined into one. Need to fix height in 'find peaks' above
 # for period in normalized_periodic_data:
 #     plt.plot(period, c='b', alpha=0.5)
 # for period in normalized_periodic_pred:
 #     plt.plot(period, c='r', alpha=0.5)
+
+# Plot mean and std dev of periodic data and predictions
+# Use after height is set correctly in 'find peaks' above
 plt.plot(mean_data, c='b', label='Mean Therapist Data')
 plt.fill_between(range(normalized_length), 
                  mean_data - std_data, 
@@ -172,6 +184,14 @@ plt.fill_between(range(normalized_length),
                  mean_pred - std_pred, 
                  mean_pred + std_pred, 
                  color='r', alpha=0.2)
+
+# Plot period plot of error using mean and std dev
+# plt.plot(mean_err, c='r', label='Mean Error')
+# plt.fill_between(range(normalized_length), 
+#                  mean_err - std_err, 
+#                  mean_err + std_err, 
+#                  color='r', alpha=0.2)
+
 plt.legend()
 plt.title("Average Periodic Data and Prediction")
 plt.xlabel('Normalized Time Steps')
