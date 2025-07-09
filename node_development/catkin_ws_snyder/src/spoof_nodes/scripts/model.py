2066dbef7b3ef5e57128c0b282da4c04263664fb
snydergi <grasnyder2001@gmail.com>
2025-07-09 16:02:29 -0500

    'Real-Time' inference test working. Will be further updated and modified for placement into exisiting codebase

--- node_development/catkin_ws_snyder/src/spoof_nodes/scripts/model.py
+++ node_development/catkin_ws_snyder/src/spoof_nodes/scripts/model.py
@@ -5,64 +5,7 @@ import torch
 import torch.nn as nn
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# First Therapist
-therapist1_datapath = '/home/cerebro/snyder_project/data/Therapist1_X2_SRA_B_07-05-2024_10-41-46.csv'
-therapist1_part1 = pd.read_csv(therapist1_datapath, 
-                            skiprows=range(1, 165636), 
-                            nrows=369050-165636)
-therapist1_part2 = pd.read_csv(therapist1_datapath, 
-                            skiprows=range(1, 452284), 
-                            nrows=654506-452284)
-therapist1_part3 = pd.read_csv(therapist1_datapath, 
-                            skiprows=range(1, 697032), 
-                            nrows=896386-697032)
-
-# Second Therapist
-therapist2_datapath = '/home/cerebro/snyder_project/data/Therapist2_X2_SRA_B_08-05-2024_14-33-51.csv'
-therapist2_part1 = pd.read_csv(therapist2_datapath,
-                            skiprows=range(1, 123920),
-                            nrows=272301-123920)
-therapist2_part2 = pd.read_csv(therapist2_datapath, 
-                       skiprows=range(1, 457108), 
-                       nrows=595193-457108)
-
-# Third Therapist
-therapist3_datapath = '/home/cerebro/snyder_project/data/Therapist3_X2_SRA_B_29-05-2024_13-41-19.csv'
-therapist3_part1 = pd.read_csv(therapist3_datapath, 
-                            skiprows=range(1, 7694), 
-                            nrows=198762-7694)
-therapist3_part2 = pd.read_csv(therapist3_datapath, 
-                            skiprows=range(1, 232681), 
-                            nrows=388280-232681)
-therapist3_part3 = pd.read_csv(therapist3_datapath, 
-                            skiprows=range(1, 417062), 
-                            nrows=606462-417062)
-
-t1 = pd.concat([therapist1_part1, therapist1_part2, therapist1_part3])
-t2 = pd.concat([therapist2_part1, therapist2_part2])
-t3 = pd.concat([therapist3_part1, therapist3_part2, therapist3_part3])
-
-train_1_len = len(t1) * 0.7
-validate_1_len = len(t1) * 0.2
-train_2_len = len(t2) * 0.7
-validate_2_len = len(t2) * 0.2
-train_3_len = len(t3) * 0.7
-validate_3_len = len(t3) * 0.2
-
-# Split testing portion of data from full dataset
-therapist1_test = t1[int(train_1_len + validate_1_len):]
-therapist2_test = t2[int(train_2_len + validate_2_len):]
-therapist3_test = t3[int(train_3_len + validate_3_len):]
-
-therapist1_data = therapist1_test[[' JointPositions_1', ' JointPositions_2', ' JointPositions_3', ' JointPositions_4']].values.astype('float32')
-therapist2_data = therapist2_test[[' JointPositions_1', ' JointPositions_2', ' JointPositions_3', ' JointPositions_4']].values.astype('float32')
-therapist3_data = therapist3_test[[' JointPositions_1', ' JointPositions_2', ' JointPositions_3', ' JointPositions_4']].values.astype('float32')
-
-j1 = []
-j2 = []
-j3 = []
-j4 = []
+import time
 
 # Model definition
 class JointModel(nn.Module):
@@ -117,6 +60,7 @@ class ModelNode:
         self.plotted = False
         self.patient_number = 1
         rospy.Subscriber("patient_data", patient_data, self.callback)
+        self.pub = rospy.Publisher("therapist_pred", therapist_pred, queue_size=10)
     def run(self):
         rospy.spin()
     def callback(self, data):
@@ -130,53 +74,23 @@ class ModelNode:
                                         data.StanceInterpolationFactor, data.BackPackAngle, data.BackPackAngularVelocity]], dtype=torch.float32)
             input_data = input_data.unsqueeze(0)  # Add batch dimension
             # Predict
+            start = time.time()
             y_pred = predict(input_data)[:, -1, :].squeeze()
-            j1.append(y_pred[0].item())
-            j2.append(y_pred[1].item())
-            j3.append(y_pred[2].item())
-            j4.append(y_pred[3].item())
+            end = time.time()
+            rospy.loginfo(f"Prediction time: {end - start:.4f} seconds")
+            # Prepare message
+            msg = therapist_pred()
+            msg.JointPositions_1 = y_pred[0].item()
+            msg.JointPositions_2 = y_pred[1].item()
+            msg.JointPositions_3 = y_pred[2].item()
+            msg.JointPositions_4 = y_pred[3].item()
+            msg.Therapist_TrueJointPosition_1 = data.Therapist_TrueJointPosition_1
+            msg.Therapist_TrueJointPosition_2 = data.Therapist_TrueJointPosition_2
+            msg.Therapist_TrueJointPosition_3 = data.Therapist_TrueJointPosition_3
+            msg.Therapist_TrueJointPosition_4 = data.Therapist_TrueJointPosition_4
+            # Publish message
+            self.pub.publish(msg)
             # rospy.loginfo(f"Predicted joint positions: {y_pred}")
-            if len(j1) > 5000 and not self.plotted:
-                self.plotted = True
-                fig = plt.figure(figsize=(12, 6))
-                fig.suptitle(f"Patient {self.patient_number} Testing: Therapist Predictions from Patient Data")
-                plt.subplot(2, 2, 1)
-                plt.plot(therapist1_data[:, 0], c='b', label=f'True Therapist Data')
-                plt.plot(j1, c='r', linestyle='--', label=f'Predicted Therapist Data')
-                plt.xlim(0, 7500)
-                plt.xlabel('Time Steps (~4ms)')
-                plt.ylabel('Joint Positions (Radians)')
-                plt.legend()
-                plt.title("Joint 1")
-
-                plt.subplot(2, 2, 2)
-                plt.plot(therapist1_data[:, 1], c='b', label=f'True Therapist Data')
-                plt.plot(j2, c='r', linestyle='--', label=f'Predicted Therapist Data')
-                plt.xlim(0, 7500)
-                plt.xlabel('Time Steps (~4ms)')
-                plt.ylabel('Joint Positions (Radians)')
-                plt.legend()
-                plt.title("Joint 2")
-
-                plt.subplot(2, 2, 3)
-                plt.plot(therapist1_data[:, 2], c='b', label=f'True Therapist Data')
-                plt.plot(j3, c='r', linestyle='--', label=f'Predicted Therapist Data')
-                plt.xlim(0, 7500)
-                plt.xlabel('Time Steps (~4ms)')
-                plt.ylabel('Joint Positions (Radians)')
-                plt.legend()
-                plt.title("Joint 3")
-
-                plt.subplot(2, 2, 4)
-                plt.plot(therapist1_data[:, 3], c='b', label=f'True Therapist Data')
-                plt.plot(j4, c='r', linestyle='--', label=f'Predicted Therapist Data')
-                plt.xlim(0, 7500)
-                plt.xlabel('Time Steps (~4ms)')
-                plt.ylabel('Joint Positions (Radians)')
-                plt.legend()
-                plt.title("Joint 4")
-                
-                plt.show()
 
 if __name__ == '__main__':
     try:
