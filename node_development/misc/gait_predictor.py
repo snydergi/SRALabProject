3a9f8b468ae4c68678f412ce804365601a242328
snydergi <grasnyder2001@gmail.com>
2025-07-14 17:03:59 -0500

    Updated dynamic reconfigure to better switch models

--- node_development/misc/gait_predictor.py
+++ node_development/misc/gait_predictor.py
@@ -14,6 +14,7 @@ import torch.nn as nn
 import dynamic_reconfigure.client
 from dynamic_reconfigure.server import Server
 from CORC.cfg import lstm_model_paramsConfig
+import yaml
 
 
 # Model definition
@@ -31,24 +32,38 @@ class JointModel(nn.Module):
 class ModelNode:
     def __init__(self):
         rospy.init_node('model_node', anonymous=True)
-        self.server = Server(lstm_model_paramsConfig, self.reconfigure_callback)
+
+        # Initialize RingBuffer
+        self.buffer = RingBuffer(50)
+
+        # Load model configurations
+        config_path = os.path.join(os.path.dirname(__file__), 'lstm_models/model_configs.yaml')
+        with open(config_path) as f:
+            self.model_configs = yaml.safe_load(f)['models']
+
+        # Load default model config
+        self.current_model = 'trial5'
+        self.model = None
+        self.input_slice = None
+        self.load_model(self.current_model)
 
         # Initialize parameters
         self.init_time = time.time()
         self.previous_pred = [None, None, None, None]
         self.config = {
             'pred_diff_threshold': 0.05,
-            'model_path': '/home/cerebro/snyder_project/SRALabProject/lstm_BigData/trial5/lstm_model_epoch198.pth'
+            'model_path': '/home/cerebro/catkin_ws/src/CANOpenRobotController/python/lstm_models/lstm_trial5.pt'
         }
-        self.model_path = '/home/cerebro/snyder_project/SRALabProject/lstm_BigData/trial5/lstm_model_epoch198.pth'
+        self.model_path = '/home/cerebro/catkin_ws/src/CANOpenRobotController/python/lstm_models/lstm_trial5.pt'
         self.skipped_pred_count = 0
 
-        # Initialize RingBuffer
-        self.buffer = RingBuffer(50)
+        # Initialize dynamic reconfigure
+        self.server = Server(lstm_model_paramsConfig, self.reconfigure_callback)
 
         # Load model
-        self.model = JointModel()
-        self.model.load_state_dict(torch.load(self.model_path))
+        # self.model = JointModel()
+        # self.model.load_state_dict(torch.load(self.model_path))
+        self.model = torch.jit.load(self.model_path)
         self.model.eval()
 
         # Prepare Subscriber, Synchronizer, and Publisher
@@ -62,25 +77,53 @@ class ModelNode:
     def run(self):
         rospy.spin()
 
+    def load_model(self, model_name):
+        if model_name not in self.model_configs:
+            rospy.logerr(f"Model {model_name} not found in configurations.")
+            return False
+        config = self.model_configs[model_name]
+        try:
+            model_path = os.path.join(os.path.dirname(__file__), config['path'])
+            self.model = torch.jit.load(model_path)
+            self.model.eval()
+
+            self.input_slice = config['input_slice']
+            self.current_model = model_name
+            self.buffer.clear()
+            return True
+        except Exception as e:
+            rospy.logerr(f"Failed to load model {model_name} from {model_path}: {e}")
+            self.model = None
+            self.input_slice = None
+            return False
+
     def predict(self, x):
         y_pred = self.model(x)
         return y_pred
 
     def reconfigure_callback(self, config, level):
-        rospy.loginfo("""Reconfigure Request: {pred_diff_threshold}, {model_path}""".format(**config))
-
-        # Reload model if path has changed
-        if hasattr(self, 'config') and self.config is not None:
-            if config['model_path'] != self.config['model_path']:
-                rospy.loginfo("Model path changed to: {}".format(config['model_path']))
-                self.model_path = config['model_path']
-                # Load new model
-                self.model.load_state_dict(torch.load(self.model_path))
-                self.model.eval()
-
-        self.config = config
-
-        return config
+        rospy.loginfo("""Reconfigure Request: {pred_diff_threshold}, {model_type}""".format(**config))
+        model_mapping = {
+            0: "trial4",
+            1: "trial5",
+            2: "trial6"
+        }
+        try: 
+            if config['model_type'] in model_mapping:
+                model_name = model_mapping[config['model_type']]
+                if model_name != self.current_model:
+                    if self.load_model(model_name):
+                        rospy.loginfo(f"Changing model to {model_name}")
+                    else:
+                        config['model_type'] = self.current_model  # Revert to previous model if loading fails
+                        rospy.logerr(f"Failed to load model {model_name}, reverting to {self.current_model}")
+                self.config['pred_diff_threshold'] = config['pred_diff_threshold']
+                self.input_slice = self.model_configs[model_name]['input_slice']
+                self.buffer.clear()
+            return config
+        except Exception as e:
+            rospy.logerr(f"Error in reconfigure callback: {e}")
+            return config
 
     def callback(self, js_data, si_data):
         try:
@@ -88,8 +131,16 @@ class ModelNode:
             data = torch.tensor([js_data.position[0], js_data.position[1], js_data.position[2], js_data.position[3],
                                 js_data.velocity[0], js_data.velocity[1], js_data.velocity[2], js_data.velocity[3],
                                 si_data.treadmill, js_data.position[4], js_data.velocity[4]], dtype=torch.float32)
+            
+            # Model-specific slicing
+            if self.current_model == 'trial4':
+                data = data[:8]
+
+            # Slice the data according to the input slice
+            data = data[self.input_slice]
+            # Append to buffer
             self.buffer.append(data)
-            if len(self.buffer.get()) < self.buffer.max:
+            if self.buffer.is_full is False:
                 pass # Not enough data to make a prediction
             else:
                 # Prepare input data
