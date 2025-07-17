142b2490f31734ab32c4dd2c5a240810021c4ae9
Grayson <grasnyder2001@gmail.com>
2025-07-17 15:37:58 -0500

    Added inference studies from gpu transfer

--- node_development/misc/inference_time_study.py
+++ node_development/misc/inference_time_study.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 # Read inference times from CSV file
 inference_times = []
-with open('/home/gis/SRALab_Data/ExperimentDay1/time_inference_1752606480.2458458.csv', 'r') as file:
+with open('/home/gis/SRALab_Data/ExperimentDay2/t4_time_inference_1752781794.4837697.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        if row[1] == 'trial6':
+        if row[1] == 'trial4':
             inference_times.append(float(row[2]))
 
 inference_times = np.array(inference_times)
@@ -18,5 +18,5 @@ plt.hist(inference_times, bins=25, alpha=0.7, color='blue')
 plt.xlabel('Inference Time (Seconds)')
 plt.ylabel('# of Occurrances (Log Scale)')
 plt.yscale('log')
-plt.title(f'Trial6 Model, Sample of {len(inference_times)} Inference Times, Mean: {np.mean(inference_times):.5f}, Max: {inference_times.max():.5f}, Min: {inference_times.min():.5f}')
+plt.title(f'Trial4 Model, Sample of {len(inference_times)} Inference Times, Mean: {np.mean(inference_times):.5f}, Max: {inference_times.max():.5f}, Min: {inference_times.min():.5f}')
 plt.show()
