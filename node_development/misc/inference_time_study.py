2d3d08feec9d3335f002bf9122a1150ac52f42b3
Grayson <grasnyder2001@gmail.com>
2025-07-17 14:17:48 -0500

    Added inference time histograms from tuesday experiment

--- node_development/misc/inference_time_study.py
+++ node_development/misc/inference_time_study.py
@@ -5,10 +5,11 @@ import pandas as pd
 
 # Read inference times from CSV file
 inference_times = []
-with open('/home/cerebro/snyder_project/SRALabProject/node_development/misc/inference_time.csv', 'r') as file:
+with open('/home/gis/SRALab_Data/ExperimentDay1/time_inference_1752606480.2458458.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        inference_times.append(float(row[0]))
+        if row[1] == 'trial6':
+            inference_times.append(float(row[2]))
 
 inference_times = np.array(inference_times)
 
@@ -16,6 +17,6 @@ fig = plt.figure(figsize=(12,6), )
 plt.hist(inference_times, bins=25, alpha=0.7, color='blue')
 plt.xlabel('Inference Time (Seconds)')
 plt.ylabel('# of Occurrances (Log Scale)')
-# plt.yscale('log')
-plt.title(f'Sample of {len(inference_times)} Inference Times, Mean: {np.mean(inference_times)}, Max: {inference_times.max()}, Min: {inference_times.min()}')
+plt.yscale('log')
+plt.title(f'Trial6 Model, Sample of {len(inference_times)} Inference Times, Mean: {np.mean(inference_times):.5f}, Max: {inference_times.max():.5f}, Min: {inference_times.min():.5f}')
 plt.show()
