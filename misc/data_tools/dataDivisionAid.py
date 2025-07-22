e5da92d4bf84b77b1d891cba650dbe51f5a446a9
Grayson <grasnyder2001@gmail.com>
2025-07-22 14:27:00 -0500

    Data preprocessing for P/T 11

--- misc/data_tools/dataDivisionAid.py
+++ misc/data_tools/dataDivisionAid.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-patient_file = '/home/gis/SRALab_Data/Patient4_X2_SRA_A_31-07-2024_11-25-24.csv'
-therapist_file = '/home/gis/SRALab_Data/Therapist4_X2_SRA_B_31-07-2024_11-29-20.csv'
+patient_file = '/home/gis/SRALab_Data/Patient11_X2_SRA_A_19-09-2024_09-37-01.csv'
+therapist_file = '/home/gis/SRALab_Data/Therapist11_X2_SRA_B_19-09-2024_09-37-07.csv'
 
 patient_data_raw = pd.read_csv(patient_file, low_memory=False)
 therapist_data_raw = pd.read_csv(therapist_file, low_memory=False)
@@ -21,19 +21,19 @@ therapist_button_data = therapist_data_raw[' greenButton'].values.astype('float3
 patient_button_data = patient_data_raw[' greenButton'].values.astype('float32')
 
 ####################### FOR SYNCHING DATA #########################
-# index_to_match = 606459
+index_to_match = 787869
 
-# # # If therapist determines
-# # print(f'Time to match: {therapist_data[index_to_match, 1]}')
-# # for k in range(len(patient_data[:,1])):
-# #     if np.round(patient_data[k, 1], 1) == np.round(therapist_data[index_to_match, 1], 1):
-# #         print(f'Possible index found at {k}, time {patient_data[k, 1]}')
+# # If therapist determines
+print(f'Time to match: {therapist_data[index_to_match, 1]}')
+for k in range(len(patient_data[:,1])):
+    if np.round(patient_data[k, 1], 1) == np.round(therapist_data[index_to_match, 1], 1):
+        print(f'Possible index found at {k}, Time: {patient_data[k, 1]}')
 
-# # If patient determines
+# # # If patient determines
 # print(f'Time to match: {patient_data[index_to_match, 1]}')
 # for k in range(len(therapist_data[:,1])):
 #     if np.round(therapist_data[k, 1], 1) == np.round(patient_data[index_to_match, 1], 1):
-#         print(f'Possible index found at {k}, time {therapist_data[k, 1]}')
+#         print(f'Possible index found at {k}, Time: {therapist_data[k, 1]}')
 ################################################################
 
 
@@ -44,42 +44,42 @@ patient_button_data = patient_data_raw[' greenButton'].values.astype('float32')
 #     if i > 0:
 #         # # For start of episodes
 #         if therapist_button_data[i] == 1 and therapist_button_data[i-1] == 0:
-#             print(f"Button pressed at index {i}, time {therapist_data[i,1]}")
+#             print(f"Button pressed at index {i}, Time: {therapist_data[i,1]}")
 #             therapist_indices.append(i)
-#         # # For when the button is pressed when data collection starts
+#         # For when the button is pressed when data collection starts
 #         # if therapist_button_data[i] == 1:
-#         #     print(f"Button pressed at index {i}, time {therapist_data[i,1]}")
+#         #     print(f"Button pressed at index {i}, Time: {therapist_data[i,1]}")
 #         #     break
 #         # # For end of episodes
 #         if therapist_button_data[i] == 0 and therapist_button_data[i-1] == 1:
-#             print(f"Button unpressed at index {i}, time {therapist_data[i,1]}")
+#             print(f"Button unpressed at index {i}, Time: {therapist_data[i,1]}")
 
-# FOR PATIENT DATA
+# # # FOR PATIENT DATA
 # patient_indices = []
 # for j in range(len(patient_button_data)):
 #     if j > 0:
 #         # For start of episodes
 #         if patient_button_data[j] == 1 and patient_button_data[j-1] == 0:
-#             print(f"Button pressed at index {j}, time {patient_data[j,1]}")
+#             print(f"Button pressed at index {j}, Time: {patient_data[j,1]}")
 #             patient_indices.append(j)
 #         # For when the button is pressed when data collection starts
 #         # if patient_button_data[i] == 1:
-#         #     print(f"Button pressed at index {i}, time {patient_data[j,1]}")
+#         #     print(f"Button pressed at index {i}, Time: {patient_data[j,1]}")
 #         #     break
 #         # For end of episodes
 #         if patient_button_data[j] == 0 and patient_button_data[j-1] == 1:
-#             print(f"Button unpressed at index {j}, time {patient_data[j,1]}")
+#             print(f"Button unpressed at index {j}, Time: {patient_data[j,1]}")
 
-fig = plt.figure(figsize=(12, 6))
-plt.plot(therapist_data[:,1], therapist_data[:,0], label='Therapist Joint Positions', c='r')
-plt.plot(patient_data[:,1], patient_data[:,0], label='Patient Joint Positions', c='b')
-plt.plot(therapist_data[:,1], therapist_data_raw[' greenButton'].values.astype('float32'), label='Green Button Value T', c='green')
-plt.plot(patient_data[:,1], patient_data_raw[' greenButton'].values.astype('float32'), label='Green Button Value P', c='yellow')
-plt.xlabel('Time (s)')
-plt.ylabel('Joint Positions, Button Values')
-plt.title('Patient and Therapist Joint Positions (J13) with Button Values')
-plt.legend()
-plt.show()
+# fig = plt.figure(figsize=(12, 6))
+# plt.plot(therapist_data[:,1], therapist_data[:,0], label='Therapist Joint Positions', c='r')
+# plt.plot(patient_data[:,1], patient_data[:,0], label='Patient Joint Positions', c='b')
+# plt.plot(therapist_data[:,1], therapist_data_raw[' greenButton'].values.astype('float32'), label='Green Button Value T', c='green')
+# plt.plot(patient_data[:,1], patient_data_raw[' greenButton'].values.astype('float32'), label='Green Button Value P', c='yellow')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Joint Positions, Button Values')
+# plt.title('Patient and Therapist Joint Positions (J13) with Button Values')
+# plt.legend()
+# plt.show()
 ###############################################################
 
 # print(therapist_data[0, 1])
