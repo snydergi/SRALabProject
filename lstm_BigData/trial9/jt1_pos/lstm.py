84032f80d8eb4ccc6bafc1c1867d254f53d0d177
Grayson <grasnyder2001@gmail.com>
2025-08-07 14:02:41 -0500

    Fixed output size

--- lstm_BigData/trial9/jt1_pos/lstm.py
+++ lstm_BigData/trial9/jt1_pos/lstm.py
@@ -55,7 +55,7 @@ class JointModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.lstm = nn.LSTM(input_size=8, hidden_size=50, num_layers=1, batch_first=True)
-        self.linear = nn.Linear(50, 4)
+        self.linear = nn.Linear(50, 1)
     def forward(self, x):
         x, _ = self.lstm(x)
         x = self.linear(x)
