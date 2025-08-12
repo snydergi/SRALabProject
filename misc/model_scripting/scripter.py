323542b12ef86b072b2f814d3bb0ae5685135bbd
Grayson <grasnyder2001@gmail.com>
2025-08-12 10:32:49 -0500

    Stacked model with offline inference testing

--- misc/model_scripting/scripter.py
+++ misc/model_scripting/scripter.py
@@ -23,20 +23,71 @@ import torch.nn as nn
 #         x = self.linear(x)
 #         return x
 
-# LSTM BigData Trial 6 Model
+# # LSTM BigData Trial 6 Model
+# class JointModel(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.lstm = nn.LSTM(input_size=11, hidden_size=50, num_layers=4, batch_first=True, dropout=0.4)
+#         self.dense = nn.Linear(50, 128)
+#         self.linear = nn.Linear(128, 4)
+#     def forward(self, x):
+#         x, _ = self.lstm(x)
+#         x = self.dense(x)
+#         x = self.linear(x)
+#         return x
+
+# Joint Model (8-to-1) for Stacking Test
 class JointModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=11, hidden_size=50, num_layers=4, batch_first=True, dropout=0.4)
-        self.dense = nn.Linear(50, 128)
-        self.linear = nn.Linear(128, 4)
+        self.lstm = nn.LSTM(input_size=8, hidden_size=50, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(50, 1)
     def forward(self, x):
         x, _ = self.lstm(x)
-        x = self.dense(x)
         x = self.linear(x)
         return x
+    
+class StackedModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Initialize models
+        self.j1p = JointModel()
+        self.j2p = JointModel()
+        self.j3p = JointModel()
+        self.j4p = JointModel()
+        self.j1v = JointModel()
+        self.j2v = JointModel()
+        self.j3v = JointModel()
+        self.j4v = JointModel()
 
-model = JointModel()
-model.load_state_dict(torch.load('../../lstm_BigData/trial6/lstm_model_epoch194.pth'))
-model_scripted = torch.jit.script(model)
-model_scripted.save('scripts/lstm_trial6.pt')
+        # Load pretrained weights
+        self.j1p.load_state_dict(torch.load('../../lstm_BigData/trial9/jt1_pos/lstm_model_epoch143.pth'))
+        self.j2p.load_state_dict(torch.load('../../lstm_BigData/trial9/jt2_pos/lstm_model_epoch129.pth'))
+        self.j3p.load_state_dict(torch.load('../../lstm_BigData/trial9/jt3_pos/lstm_model_epoch140.pth'))
+        self.j4p.load_state_dict(torch.load('../../lstm_BigData/trial9/jt4_pos/lstm_model_epoch147.pth'))
+        self.j1v.load_state_dict(torch.load('../../lstm_BigData/trial9/jt1_vel/lstm_model_epoch135.pth'))
+        self.j2v.load_state_dict(torch.load('../../lstm_BigData/trial9/jt2_vel/lstm_model_epoch149.pth'))
+        self.j3v.load_state_dict(torch.load('../../lstm_BigData/trial9/jt3_vel/lstm_model_epoch148.pth'))
+        self.j4v.load_state_dict(torch.load('../../lstm_BigData/trial9/jt4_vel/lstm_model_epoch149.pth'))
+
+    def forward(self, x):
+        x1p = self.j1p(x)
+        x2p = self.j2p(x)
+        x3p = self.j3p(x)
+        x4p = self.j4p(x)
+        x1v = self.j1v(x)
+        x2v = self.j2v(x)
+        x3v = self.j3v(x)
+        x4v = self.j4v(x)
+
+        # Contatenate outputs along the last dimension ([batch, seq_len, 8])
+        return torch.cat([x1p, x2p, x3p, x4p, x1v, x2v, x3v, x4v], dim=-1)
+
+# model = JointModel()
+# model.load_state_dict(torch.load('../../lstm_BigData/trial6/lstm_model_epoch194.pth'))
+# model_scripted = torch.jit.script(model)
+# model_scripted.save('scripts/lstm_trial6.pt')
+
+model = StackedModel()
+model_sctipted = torch.jit.script(model)
+model_sctipted.save('scripts/stacked_model.pt')
---
+++ misc/stacked_model_testing/test_stacked.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import time
+
+# Set plotting parameters
+plt.rcParams['svg.fonttype'] = 'none'
+
+# Load the data
+# DATA ORDER:
+# p_jt_pos1, p_jt_pos2, p_jt_pos3, p_jt_pos4, p_jt_vel1, p_jt_vel2, p_jt_vel3, p_jt_vel4, p_SIF, p_bp_pos, p_bp_vel,
+# t_jt_pos1, t_jt_pos2, t_jt_pos3, t_jt_pos4, t_jt_vel1, t_jt_vel2, t_jt_vel3, t_jt_vel4
+test_1 = pd.read_csv('../../lstm_BigData/data/test_set_1.csv', skiprows=0).values
+test_2 = pd.read_csv('../../lstm_BigData/data/test_set_2.csv', skiprows=0).values
+test_3 = pd.read_csv('../../lstm_BigData/data/test_set_3.csv', skiprows=0).values
+
+def create_dataset(dataset, lookback, step=1):
+    """Transform a time series into a prediction dataset
+    
+    Args:
+        dataset: A numpy array of time series data
+        lookback: Size of window for prediction
+    """
+    X, y = [], []
+    for i in range(0, len(dataset)-lookback, step):
+        feature = dataset[i:i+lookback, :8]  # Feature is patient data, if input size 8
+        target = dataset[i+1:i+lookback+1, -8:]  # Target is therapist data
+        X.append(feature)
+        y.append(target)
+    X = np.array(X)
+    y = np.array(y)
+    return torch.tensor(X, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
+
+lookback, step = 50, 5
+patient_number = 1 # Set based on which patient data is being tested
+test = test_1 if patient_number == 1 else test_2 if patient_number == 2 else test_3
+predicted_target = -2 # Index based on data order above
+pIndex = 4 # Index of patient joint to plot against therapist joint (0-3 for patient joints 1-4)
+X_test, y_test = create_dataset(test, lookback=lookback)
+
+# Load model
+model = torch.jit.load('../model_scripting/scripts/stacked_model.pt')
+model.eval()
+loss_fn = nn.MSELoss()
+
+# Create DataLoader
+loader = data.DataLoader(data.TensorDataset(X_test, y_test), shuffle=False, batch_size=1)
+
+# Testing
+with torch.no_grad():
+    test_rmse = []
+    pred_times = []
+    # Iterate through DataLoader
+    for X_batch, y_batch in loader:
+        pred_start = time.time()
+        y_pred = model(X_batch)
+        pred_end = time.time()
+        pred_times.append(pred_end - pred_start)
+        # print(f'Prediction time for batch: {pred_times[-1]:.6f} seconds')
+
+print(f'Average prediction time per batch: {np.mean(pred_times):.6f} seconds')
