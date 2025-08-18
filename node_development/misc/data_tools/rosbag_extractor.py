d39654c472852925df3729e8b00cf4379d87fea4
Grayson <grasnyder2001@gmail.com>
2025-08-18 15:45:39 -0500

    Velocity comparison graph of predicted vs num diff

--- node_development/misc/data_tools/rosbag_extractor.py
+++ node_development/misc/data_tools/rosbag_extractor.py
@@ -1,6 +1,6 @@
 from bagpy import bagreader
 
-bag = bagreader('/home/cerebro/catkin_ws/2025-08-06-15-42-49.bag') # Path to rosbag
-msg = bag.message_by_topic('/X2_SRA_B/custom_robot_state') # Select topic for conversion to csv
+bag = bagreader('/home/gis/SRALab_Data/StackedTestDay1/2025-08-14-16-22-37.bag') # Path to rosbag
+msg = bag.message_by_topic('/X2_SRA_A/joint_states/velocity') # Select topic for conversion to csv
 
 print(msg) # Prints path of saved csv
